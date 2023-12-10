6320889b873be757a7739c2d78006353fd2c5808
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2023-12-11 01:28:32 +0300

    unitofwork ok

---
+++ IdeWeb/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ide.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        
+
+
+        public BaseController()
+        {
+            
+        }
+    }
+}
---
+++ IdeWeb/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Ide.Utility;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ide.Web.Controllers
+{
+    public class UserController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            string a = "";
+           
+            return View();
+        }
+    }
+}
